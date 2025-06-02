94363788980709dfe36d75f32f3b9e5c9fb65d53
32bitsret <dretnan@logicaladdress.com>
2025-06-02 23:34:00 +0200

    Update class-wc-everydaymoney-api.php

--- includes/class-wc-everydaymoney-api.php
+++ includes/class-wc-everydaymoney-api.php
@@ -22,7 +22,7 @@ class WC_Everydaymoney_API {
         $this->api_base_url = rtrim( EVERYDAYMONEY_GATEWAY_API_URL, '/' );
 
         if ( $this->gateway->test_mode ) {
-            $test_api_url = defined('EVERYDAYMONEY_GATEWAY_TEST_API_URL') ? EVERYDAYMONEY_GATEWAY_TEST_API_URL : 'http://localhost:8000'; // Example test URL without /v1
+            $test_api_url = defined('EVERYDAYMONEY_GATEWAY_TEST_API_URL') ? EVERYDAYMONEY_GATEWAY_TEST_API_URL : 'http://localhost:8080'; // Example test URL without /v1
             $this->api_base_url = apply_filters( 'wc_everydaymoney_test_api_url', rtrim($test_api_url, '/') );
             $this->logger->log( 'API Handler initialized in Test Mode. API Base URL: ' . $this->api_base_url, 'debug' );
         } else {
@@ -44,7 +44,7 @@ class WC_Everydaymoney_API {
             return false;
         }
 
-        $login_url = $this->api_base_url . '/auth/business/login';
+        $login_url = $this->api_base_url . '/auth/business/token';
         $auth_string  = $this->gateway->public_key . ':' . $this->gateway->api_secret;
         $base64_token = base64_encode( $auth_string );
 
